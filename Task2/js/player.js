cf5bbc396b2d50bb1b57dca5b62a0a2eb106b56e
vdulevich <vdulevich@gloriumtech.com>
2015-10-01 15:48:50 +0300

    1. Player play button

--- Task2/js/player.js
+++ Task2/js/player.js
@@ -1,28 +1,33 @@
 function player(id) {
-	this.__id = id;
-	this.__video = null;
-	this.__playbutton = null;
+	var me = this;
+	me.__id = id;
+	me.__video = null;
+	me.__playButtonLarge = null;
+	me.__container = null;
+	me.__containerPauseCss = 'video-player--pause';
+	me.__playButtonLargeCss = 'play-button-large';
+	me.replaceClass = function(el, className, newClassName) {
+		el.className = el.className.replace(className, newClassName);
+	};
 };
 
 player.prototype.init = function(){
-	var container = document.getElementById(this.__id);
-	this.__video = container.getElementsByTagName('video')[0];
-	this.__playbutton = container.getElementsByClassName('video-play-button')[0];
-	this.__playbutton.addEventListener('click', this.onPlayClick.bind(this));
-}
-
-player.prototype.onPlayClick = function(){
-	this.togglePlayback();
+	var me = this;
+	var container = document.getElementById(me.__id);
+	me.__container = container;
+	me.__video = container.getElementsByTagName('video')[0];
+	me.__playButtonLarge = container.getElementsByClassName(me.__playButtonLargeCss)[0];
+	me.__playButtonLarge.addEventListener('click', me.togglePlayback.bind(me));
+	me.__video.addEventListener('click', me.togglePlayback.bind(me));
 }
 
 player.prototype.togglePlayback = function(){
 	if(this.__video.paused) {
-		this.__video.play();
-		
+		this.__container.className += ' ' + this.__containerPauseCss;
 	} else {
-		this.__video.pause();
+		this.replaceClass(this.__container, this.__containerPauseCss, '');
 	}
-	this.__playbutton.classList.toggle('icon-font-play');
-	this.__playbutton.classList.toggle('icon-font-pause');
+	this.__video.paused ? this.__video.play(): this.__video.pause();
 }
+
 
